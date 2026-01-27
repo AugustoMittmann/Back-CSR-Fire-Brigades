247833e0d3016e9c596d851d716538f085e3897e
AUGUSTO MITTMANN <augusto.mittmann@sap.com>
2026-01-27 16:35:31 -0300

    creating backend

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
-import express from "express";
+import express from 'express'
+import cors from 'cors'
+import brigadesRoutes from './routes/brigades.routes.js'
 
-const app = express();
+const app = express()
 
-app.get('/', function (req, res) {
-    res.send("Hello World")
+app.use(cors())
+app.use(express.json())
+
+app.use('/Brigades', brigadesRoutes)
+
+app.listen(process.env.PORT, () => {
+  console.log(`🔥 Backend rodando na porta ${process.env.PORT}`)
 })
-
-app.listen(4000);
